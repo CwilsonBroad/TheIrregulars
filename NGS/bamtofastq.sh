8f41c0e38929f991965f5faec91de69be0c737ce
Christopher Wilson <cwilson@broadinstitute.org>
2020-08-10 07:19:44 -0400

    Update bamtofastq.sh

--- NGS/bamtofastq.sh
+++ NGS/bamtofastq.sh
@@ -1,5 +1,29 @@
 #Converts bams to pair-end reads. 
 
-bedtools bamtofastq -i aln.qsort.bam \
-                      -fq aln.end1.fq \
-                      -fq2 aln.end2.fq
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 2
+#SBATCH --mem 10000
+#SBATCH -p shared
+#SBATCH -o wgetGenome_%A.out
+#SBATCH -e wgetGenome_%A.err
+#SBATCH -J wgetGenome_chupa
+#SBATCH -t 10:00:00
+
+module load bedtools2
+module load samtools
+
+Names=*.bam
+
+for name in $Names; do
+samtools sort -n $name -o $name.qsort
+done
+
+Names=*.qsort
+
+for name in $Names; do
+bedtools bamtofastq -i $name \
+                      -fq $name.R1.fq \
+                      -fq2 $name.R2.fq
+done
+echo "All done asshole"
